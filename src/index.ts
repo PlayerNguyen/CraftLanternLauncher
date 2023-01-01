21b561ee7cd6cd1a7ab4a0cbd2120abef197c51a
Player Nguyên <daudaua3782@gmail.com>
2023-01-01 20:17:22 +0700

    refactor(electron): clean up redundant space

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ app.whenReady().then(async () => {
   let registry = new IpcMainListenerRegistry();
   // TODO: Register the listener for api
   registry.subscribe();
-  
+
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
