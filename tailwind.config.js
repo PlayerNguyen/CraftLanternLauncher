04a5d535d8b3af58edbb31b0107dd5e9198dac2a
Player Nguyên <daudaua3782@gmail.com>
2023-01-02 22:11:56 +0700

    add(electron): fonts, loading effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/render/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./dist/render/**/*.{html,js,ts,jsx,tsx}",
+    "./render/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      "base-mono": "Pixeloid Mono",
+      "base-sans": "Pixeloid Sans",
+      "base-sans-bold": "Pixeloid Sans Bold",
+    },
+    extend: {
+      keyframes: {
+        floating: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "25%": { transform: "translateY(2vh)" },
+          "75%": { transform: "translateY(-2vh)" },
+        },
+      },
+      animation: {
+        floating: "floating 1s ease-in-out infinite",
+      },
+    },
+    colors: {
+      primary: {
+        white: "#fff",
+        black: "#000",
+        "dimmed-white": "#f5f5f5",
+        focus: "",
+      },
+    },
   },
   plugins: [],
 };
