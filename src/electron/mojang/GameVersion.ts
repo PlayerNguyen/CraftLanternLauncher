0b65684d63797bf2db88d8e21f75c41cee32816d
Player Nguyên <daudaua3782@gmail.com>
2023-01-09 20:34:19 +0700

    add(node): game version class to handle game version

---
+++ src/electron/mojang/GameVersion.ts
@@ -0,0 +1,126 @@
+export type GameVersionAction = "allow" | "deny";
+
+export interface GameVersionGameArguments {
+  rules: [
+    {
+      action: GameVersionAction;
+      features: { has_custom_resolution: boolean } | { is_demo_user: boolean };
+    }
+  ];
+  value: string | [string];
+}
+
+export interface GameBundleInfo {
+  sha1: string;
+  size: number;
+  url: string;
+}
+
+export interface GameVersionJvmArguments {
+  rules: [
+    {
+      action: GameVersionAction;
+      os: GameVersionOperatingSystem;
+    }
+  ];
+  value: string | [string];
+}
+
+export interface GameVersionLibraryRule {}
+
+export interface GameVersionLibrary {
+  downloads?: GameVersionLibraryDownloads;
+  name?: string;
+  rules?: GameVersionLibraryRule[];
+}
+
+export interface GameVersionLibraryDownloads {
+  artifact: GameVersionLibraryArtifact;
+}
+
+export interface GameVersionLibraryArtifact {
+  path: string;
+  sha1: string;
+  size: number;
+  url: string;
+}
+
+export interface GameVersionLibraryRule {
+  action: string;
+  os: GameVersionOperatingSystem;
+}
+
+export interface GameVersionOperatingSystem {
+  name?: string;
+  version?: RegExp;
+  arch?: string;
+}
+
+export interface GameVersionResponse {
+  arguments: {
+    game: [string | GameVersionGameArguments];
+    jvm: [string | GameVersionJvmArguments];
+  };
+
+  assetIndex: {
+    id: string;
+    sha1: string;
+    size: number;
+    totalSize: number;
+    url: string;
+  };
+
+  assets: string | number;
+  complianceLevel: string;
+
+  downloads: {
+    client: GameBundleInfo;
+    client_mappings: GameBundleInfo;
+    server: GameBundleInfo;
+    server_mappings: GameBundleInfo;
+  };
+
+  id: string;
+  javaVersion: {
+    component: string;
+    majorVersion: number;
+  };
+
+  libraries: GameVersionLibrary[];
+  logging: {
+    client: {
+      argument: string;
+      file: {
+        id: string;
+        sha1: string;
+        size: number;
+        url: string;
+      };
+      type: string | "log4j2-xml";
+    };
+  };
+
+  mainClass: string;
+  minimumLauncherVersion: number;
+  releaseTime: Date;
+  time: Date;
+  type: "release" | "snapshot" | "old_alpha";
+}
+
+export class GameVersionParser {
+  public parse(gameVersion: string): GameVersionResponse {
+    return JSON.parse(gameVersion);
+  }
+}
+
+export class GameVersion {
+  private response: GameVersionResponse;
+
+  constructor(response: GameVersionResponse) {
+    this.response = response;
+  }
+
+  
+
+}
+
