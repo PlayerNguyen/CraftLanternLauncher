3fc59ddae6e5771a21a37cafe3f349b318efe7ff
Player Nguyên <daudaua3782@gmail.com>
2023-01-09 17:28:55 +0700

    add(test): latest version test

--- test/unit/GameManifest.spec.ts
+++ test/unit/GameManifest.spec.ts
@@ -1,5 +1,6 @@
 import {
   MinecraftManifestStorage,
+  MinecraftVersion,
   VersionManifestResponse,
 } from "./../../src/electron/mojang/MinecraftVersionManifest";
 import { expect } from "chai";
@@ -17,6 +18,7 @@ describe("MinecraftManifestStorage", () => {
       }
     );
   });
+
   it("Storage has already loaded when access", async function () {
     // Getter from storage must faster than 100 milliseconds
     this.timeout(100);
@@ -24,5 +26,14 @@ describe("MinecraftManifestStorage", () => {
     expect(storage).to.not.be.empty;
     expect(storage).to.have.property("latest").and.to.not.be.empty;
     expect(storage).to.have.property("versions").and.to.not.be.empty;
+  });
+
+  it(`Latest release version match to storage manifest`, async function () {
+    const storage = await MinecraftManifestStorage.getManifest();
+    const latestReleaseVersion: MinecraftVersion | undefined =
+      await MinecraftManifestStorage.getLatestReleaseVersion();
+
+    expect(latestReleaseVersion).not.to.be.undefined;
+    expect(latestReleaseVersion?.id).to.eq(storage.latest.release);
   });
 });
