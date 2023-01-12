f35223a171bf2a18db28aabd291bd3e414dbe91e
Player Nguyên <daudaua3782@gmail.com>
2023-01-12 12:46:15 +0700

    add(test): global setup and teardown for test

---
+++ test/unit/fixtures.ts
@@ -0,0 +1,12 @@
+before(() => {
+  /**
+   * Requires environment variables for all test suites
+   */
+  require("dotenv").config();
+});
+
+after(() => {
+  /**
+   * Clean up test code here
+   */
+});
