785c03b7320de76954d6151e8dfe8c11b69ffdab
Player Nguyên <daudaua3782@gmail.com>
2023-01-29 15:50:34 +0700

    fix(test): make and remove a test output in fixtures

--- test/unit/fixtures.ts
+++ test/unit/fixtures.ts
@@ -1,13 +1,19 @@
+import chalk from "chalk";
 import { rmSync } from "fs";
 import { mkdirSync } from "fs";
 import { existsSync } from "fs";
 import { getApplicationDataPath } from "../../src/electron/AssetResolver";
+import { getTestOutputDirectory } from "./utils/file";
 before(() => {
   /**
    * Requires environment variables for all test suites
    */
   if (!existsSync(getApplicationDataPath())) {
     mkdirSync(getApplicationDataPath(), { recursive: true });
+  }
+
+  if (!existsSync(getTestOutputDirectory())) {
+    mkdirSync(getTestOutputDirectory(), { recursive: true });
   }
 });
 
@@ -17,5 +23,14 @@ after(() => {
    */
   if (existsSync(getApplicationDataPath())) {
     rmSync(getApplicationDataPath(), { force: true, recursive: true });
+  }
+
+  console.log(
+    chalk.green(
+      `Cleaning test output directory: ${getTestOutputDirectory()}...`
+    )
+  );
+  if (existsSync(getTestOutputDirectory())) {
+    rmSync(getTestOutputDirectory(), { force: true, recursive: true });
   }
 });
